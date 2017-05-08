020caaaf40209d0bbe6addb291922f36491cffe3
Gerob Kimball <gerobk@gmail.com>
2017-05-08 13:26:44 -0400

    Get the party started

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Gerob Lee Kimball</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+</head>
+<body>
+<div class="container">
+
+    <div class="col-lg-8">
+        <h1>Gerob Lee Kimball<br>
+            <small>Web Developer &amp; Geek</small>
+        </h1>
+    </div>
+    <div class="col-lg-4">
+
+    </div>
+</div>
+</body>
+</html>
